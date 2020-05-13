c4dbce4fd1bae4760a3186f051d17b619ae867e2
icowen <ian_cowen@yahoo.com>
2020-05-13 10:52:32 -0500

    Initial Commit

---
+++ 5-13.py
@@ -0,0 +1,135 @@
+'''
+Hi, Johnny! These are a couple practice problems that you can work on
+at home or in your free time. There is no single "correct" answer,
+so you can solve these problems in many different ways. If you get stuck
+on one, just move on to another problem and we can go over all these
+the next time we meet.
+'''
+
+'''
+The first thing you should do is make sure you can run this file.
+In the top right, next to the green 'run' button, click on the box
+-> Edit Configurations. Then in the top left click on the +, click python.
+Then in the top box, change 'unnamed' to 'practice problems'.
+Then click on the folder in the script path box, then find the name of 
+this file (5-13 I think), click ok, and then click ok in the button right.
+Now you should be able to press the play button and it will print
+out the print line below.
+'''
+
+print('Congratulations! You set up your first python file!')
+
+'''
+Now, let's move on to some problems!
+
+1.) Make a list with the numbers 0-10 in it and print it out.
+'''
+
+
+
+
+'''
+2.) Use a for loop and the range function to print out the numbers 
+    between 0 and 500. 
+'''
+
+
+
+
+'''
+3.) Get user input (use the input function) and print out:
+    -"You typed 1." if the user types 1, 
+    -"You typed 2." if the user types 2,
+    -"Hello" + whatever the user types in, if the user types in anything that is not a 1 or 2. 
+'''
+
+
+
+
+
+'''
+4.) Print out "Hank the tank" 10 times.
+'''
+
+
+
+
+
+
+'''
+5.) Draw the triangle using the symbol "." and the bottom row having 150 .'s.
+    It should look like:
+    .
+    ..
+    ...
+    ....
+    .....
+
+'''
+
+
+
+'''
+6.) Print out all the even numbers between 0 and 100.
+'''
+
+
+
+
+'''
+7.) Print out the square for all the numbers between 0 and 100.
+    It should start out: 0 1 4 9 16 ...
+'''
+
+
+
+'''
+8.) Make a list with 10 words in it and then print out each word using a for loop.
+'''
+
+
+
+
+''''
+9.) Make a list of 10 numbers and add them all up using a for loop.
+    Then print out that result.
+'''
+
+
+
+
+'''
+10.) Ask a user for 2 numbers, then print out the sum of those numbers.
+'''
+
+
+
+
+'''
+Super Challenge: Senator Problem
+
+Let's learn who can be a US senator. A senator must fulfill all of 
+the following 3 requirements:
+    1.) Be at least 30 years old.
+    2.) Be a US citizen for at least 9 years
+    3.) Live in the state they want to represent.
+They must meet all 3 requirements to be a senator. If they meet 0, 1, or 2,
+they are not eligible to be a senator.
+
+ex.) Puddle is 45, lived in the US her whole life, and lives in Illinois,
+        which she wants to represent, so she IS eligible to be a senator.
+        
+    Jimmy John is 45, has been a US citizen for 6 years, and lives
+    in Illinois which he wants to represent. Since Jimmy John, has not
+    been a US citizen for at least 9 years, he IS NOT eligible to 
+    be a senator.
+    
+Your task is to write a program that asks the user for 3 things:
+    1.) What is their age?
+    2.) How long they have been a US citizen?
+    3.) Do they live in the state they represent?
+
+Then the program either says:
+    -"You are eligible to be a US senator" or
+    -"You are NOT eligible to be a US senator"
+'''
